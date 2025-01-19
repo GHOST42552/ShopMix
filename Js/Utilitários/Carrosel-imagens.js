019f38e3d317b22afde4eca1aad41722dc3717df
GHOST42552 <igordanielferreira2004@gmail.com>
2025-01-19 13:00:27 -0300

    Add files via upload

---
+++ Js/Utilitários/Carrosel-imagens.js
@@ -0,0 +1,14 @@
+$('.slider').slick({
+  Boolean: true,
+  type: String,
+  default: 'ease',
+  infinite: true,
+  dots: true,
+  speed: 1000,
+  centerMode: true,
+  centerPadding: '300px',
+  arrows: true,
+  autoplay: true,
+  responsive: [{
+  }]
+});
